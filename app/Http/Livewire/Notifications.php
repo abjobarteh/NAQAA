fa06fc55c9e5b8b3c039b14f28d8b673e9b47fd8
Biran Jobe <biranjobe69@gmail.com>
2021-12-04 12:14:27 +0000

    updated notifications functionality

--- app/Http/Livewire/Notifications.php
+++ app/Http/Livewire/Notifications.php
@@ -9,8 +9,14 @@ class Notifications extends Component
     public function render()
     {
         $notifications = auth()->user()->roles[0]->unreadNotifications;
-        // dd($notifications);
 
         return view('livewire.notifications', compact('notifications'))->extends('layouts.admin');
+    }
+
+    public function markAllRead()
+    {
+        auth()->user()->roles[0]->unreadNotifications->markAsRead();
+
+        return redirect(route('notifications'));
     }
 }
